a3027e3ccdbc08c275852e9765c8d7cbfc7ae632
Patrik Hermansson <hermansson.patrik@gmail.com>
2023-04-05 16:22:56 +0200

    License frame done

--- WillItRain/app/src/main/java/com/paheco/willitrain/MainActivity.kt
+++ WillItRain/app/src/main/java/com/paheco/willitrain/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.paheco.willitrain
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.widget.Toolbar
@@ -8,9 +9,7 @@ import androidx.appcompat.widget.Toolbar
 class MainActivity : AppCompatActivity() {
     companion object {
         var logTag = "willitrainlog"
-        //var smhiURL = "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/12.75277/lat/58.38920/data.json"
-        var smhiURLP1 =
-            "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/"
+        var smhiURLP1 = "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/"
         var smhiLon = "12.75277"
         var smhiLatTxt = "/lat/"
         var smhiLat = "58.38920"
@@ -22,9 +21,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val toolbar = findViewById(R.id.toolbar) as Toolbar?
-                toolbar?.title = "Androidly"
-                toolbar?.setNavigationOnClickListener {
-                    supportFragmentManager.beginTransaction().replace(R.id.fragmentContainerView2, InfoFragment()).commit()
-                }
+        toolbar?.setNavigationOnClickListener {
+            supportFragmentManager.beginTransaction().replace(R.id.fragmentContainerView2, InfoFragment()).commit()
+        }
+/*
+        val file_name = "LICENSE.md"
+        val bufferReader = application.assets.open(file_name).bufferedReader()
+        val data = bufferReader.use {
+            it.readText()
+        }
+        Log.d(logTag,  data)
+
+ */
     }
+
 }
